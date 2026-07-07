d941c73591dfa637e4fc27f1f4373d31e14823ca
saba kiladze <kiladze.saba2007@gmail.com>
2026-07-07 23:32:37 +0400

    Add project files.

---
+++ management-ui-library/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace management_ui_library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
